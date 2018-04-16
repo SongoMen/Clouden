b84311e93479dc7f15b39b1925f80386b5dcfc9d
SongoMen <songomengaming@gmail.com>
2018-04-16 22:13:23 +0200

    firebase connect and login screen

--- src/App.js
+++ src/App.js
@@ -1,51 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios, { post } from 'axios';
-import ReactDOM from 'react-dom';
-import Dropzone from 'react-dropzone'
+import React, { Component } from "react";
 
-
-class App extends React.Component {
-
+import LoginScreen from "./Loginscreen";
+class App extends Component {
   constructor(props) {
     super(props);
-    this.state ={
-      file:null
-    }
-    this.onFormSubmit = this.onFormSubmit.bind(this)
-    this.onChange = this.onChange.bind(this)
-    this.fileUpload = this.fileUpload.bind(this)
+    this.state = {
+      loginPage: [],
+      uploadScreen: []
+    };
   }
-  onFormSubmit(e){
-    e.preventDefault() // Stop form submit
-    this.fileUpload(this.state.file).then((response)=>{
-      console.log(response.data);
-    })
+  componentWillMount() {
+    var loginPage = [];
+    loginPage.push(<LoginScreen appContext={this} />);
+    this.setState({
+      loginPage: loginPage
+    });
   }
-  onChange(e) {
-    this.setState({file:e.target.files[0]})
-  }
-  fileUpload(file){
-    const url = 'http://localhost:8080';
-    const formData = new FormData();
-    formData.append('file',file)
-    const config = {
-        headers: {
-            'content-type': 'multipart/form-data'
-        }
-    }
-    return  post(url, formData,config)
-  }
-
   render() {
-    return (
-      <form onSubmit={this.onFormSubmit}>
-        <h1>File Upload</h1>
-        <input type="file" onChange={this.onChange} />
-        <button type="submit">Upload</button>
-      </form>
-   )
+    return <div className="App">{this.state.loginPage}</div>;
   }
 }
 
