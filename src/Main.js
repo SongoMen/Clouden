701cb648203b8f7eb19047960bc16deda27016a6
SongoMen <songomengaming@gmail.com>
2018-05-06 16:30:30 +0200

    landing page update sections

--- src/Main.js
+++ src/Main.js
@@ -48,11 +48,11 @@ class Main extends Component {
 					<div className="arrow-circle">
 						<svg 
 						xmlns="http://www.w3.org/2000/svg" 
-						xmlnsXLINK="http://www.w3.org/1999/xlink" 
+						xmlnsXlink="http://www.w3.org/1999/xlink" 
 						version="1.1" 
 						id="Capa_1" x="0px" y="0px" 
 						viewBox="0 0 21.825 21.825"
-						xmlSPACE="preserve" 
+						xmlSpace="preserve" 
 						width="512px" 
 						height="512px">
 						<path d="M16.791,13.254c0.444-0.444,1.143-0.444,1.587,0c0.429,0.444,0.429,1.143,0,1.587l-6.65,6.651  c-0.206,0.206-0.492,0.333-0.809,0.333c-0.317,0-0.603-0.127-0.81-0.333l-6.65-6.651c-0.444-0.444-0.444-1.143,0-1.587  s1.143-0.444,1.587,0l4.746,4.762V1.111C9.791,0.492,10.299,0,10.918,0c0.619,0,1.111,0.492,1.111,1.111v16.904L16.791,13.254z" fill="#9fa4af"/>
@@ -60,7 +60,14 @@ class Main extends Component {
 					</div>
 				</div>
 				<div className="sections">
-					<h1>dsadsa</h1>
+					<div className="text">
+						<span className="about-line"></span>
+						<h2>ABOUT US</h2>
+						<h1>Who we are?</h1>
+						<p className="description">
+							sdadsadsad
+						</p>
+					</div>
 					<div className="bottomBar">
 						ffsafas
 					</div>
