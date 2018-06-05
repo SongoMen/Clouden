fbc7da7fe378d92b5b46fada34a8d308cfa95b76
SongoMen <songomengaming@gmail.com>
2018-06-06 00:15:06 +0200

    panel update

---
+++ src/PanelPage/Dashboard/Dashboard.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import {logout} from '../../helpers/auth'
+
+import './Dashboard.css'
+import PanelBars from '../PanelBars.js' 
+
+export default class Dashboard extends Component{
+
+    render(){
+        return(
+            <div className="Panel-dashboard">
+                <PanelBars content = {[                
+                    <div className="panel-sections" key={1}>
+                        <div className="panel-sections__disk">
+                            <h1>2.3/5 GB</h1>
+                        </div>
+                        <div className="panel-sections__account">
+                            <h1>2.3/5 GB</h1>
+                            <p onClick = {() => {
+                                logout()
+                            }}>Logout</p>
+                        </div>
+                    </div>
+                ]}/>
+            </div>
+            
+        )
+    }
+
+}
--- src/PanelPage/PanelBars.js
+++ src/PanelPage/PanelBars.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import { logout } from '../helpers/auth'
 import * as firebase from 'firebase';
 
-import './Panel.css'
+import './PanelBars.css'
 
-class Panel extends Component {
+class PanelBars extends Component {
 
   /*<p onClick = {() => {
     logout()
@@ -20,17 +20,10 @@ class Panel extends Component {
         <aside className="panel-leftbar">
           <h4 className="user">Hello {user}</h4>
         </aside>
-        <div className="panel-sections">
-          <div className="panel-sections__disk">
-              <h1>2.3/5 GB</h1>
-          </div>
-          <div className="panel-sections__account">
-              <h1>2.3/5 GB</h1>
-          </div>
-        </div>
+        {this.props.content}
       </div>
     );
   }
 }
 
-export default Panel;
+export default PanelBars;
