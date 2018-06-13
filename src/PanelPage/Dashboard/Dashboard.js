abecccda5c31463224b7f9f5c256832a20b28b8a
SongoMen <songomengaming@gmail.com>
2018-06-13 21:38:26 +0200

    added file input

--- src/PanelPage/Dashboard/Dashboard.js
+++ src/PanelPage/Dashboard/Dashboard.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
-import {logout} from '../../helpers/auth'
+import {logout} from '../../helpers/auth';
+import * as firebase from 'firebase';
 
 import './Dashboard.css'
 import Panel from '../Panel.js' 
 
+var storageRef = firebase.storage().ref();
+
 export default class Dashboard extends Component{
 
+    handleChangeFile(event) {
+        var file = event.target.files[0];
+        let formData = new FormData();
+        formData.append('file', file);
+      }
     render(){
         return(
             <div className="Panel-dashboard">
@@ -13,6 +21,8 @@ export default class Dashboard extends Component{
                     <div className="panel-sections" key={1}>
                         <div className="panel-sections__disk">
                             <h1>Space Usage</h1>
+                            <input type="file"/>
+                            <input type="submit" onSubmit={this.handleUploadImage}/>
                         </div>
                         <div className="panel-sections__account">
                             <h1>2.3/5 GB</h1>
@@ -22,8 +32,7 @@ export default class Dashboard extends Component{
                         </div>
                     </div>
                 ]}/>
-            </div>
-            
+            </div>  
         )
     }
 
