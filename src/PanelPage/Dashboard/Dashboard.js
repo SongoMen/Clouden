8dda72d0093c68985dd565be55416d957a360cb0
SongoMen <songomengaming@gmail.com>
2018-07-09 20:19:57 +0200

    added loading screen

--- src/PanelPage/Dashboard/Dashboard.js
+++ src/PanelPage/Dashboard/Dashboard.js
@@ -21,9 +21,11 @@ export default class Dashboard extends Component{
             spaceInBytes:"",
             avatar: '',
             avatarURL: '',
-            uploadTab:false
+            uploadTab:false,
+            loading:true
         }
         this.handleUploadSuccess = this.handleUploadSuccess.bind(this);
+        this.handleUploadStart = this.handleUploadStart.bind(this);
     }
 
     handleChangeUsername = (event) => {
@@ -127,6 +129,14 @@ export default class Dashboard extends Component{
                 console.log(key)
             });
         });
+        setTimeout(() => {
+            
+        if(this.state.totalSize !== 0){
+            this.setState({
+                loading:false
+            })
+        }
+        }, 1500);
     };
 
     render(){
@@ -141,7 +151,21 @@ export default class Dashboard extends Component{
             }
         }
         var user = firebase.auth().currentUser.displayName;
-        return(
+        return this.state.loading === true ? 
+            <div className="loader loader--style3" title="2">
+              <svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                width="60px" height="60px" viewBox="0 0 50 50" xmlSpace="preserve">
+              <path fill="#dc323c" d="M43.935,25.145c0-10.318-8.364-18.683-18.683-18.683c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615c8.072,0,14.615,6.543,14.615,14.615H43.935z">
+                <animateTransform attributeType="xml"
+                  attributeName="transform"
+                  type="rotate"
+                  from="0 25 25"
+                  to="360 25 25"
+                  dur="0.6s"
+                  repeatCount="indefinite"/>
+                </path>
+              </svg>
+            </div> : (
             <Panel content = {[       
                 <div className="dashboard" key={1}>         
                     <div className="sections">
